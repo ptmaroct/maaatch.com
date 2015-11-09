e998113d80a16d5c6272ce7139a360410699caf7
Andrew Tolvstad <tolvstaa@oregonstate.edu>
2015-11-09 02:31:17 -0500

    Moved to Bootstrap

---
+++ htdocs/common/common.php
@@ -0,0 +1,57 @@
+<?php
+function head_tags() {
+echo '
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+<link rel="stylesheet" href="index.css">
+';
+}
+	
+function navelem($name, $url, $active) {
+echo '<li';
+echo (($active)?' class="active"':'').'>';
+echo '<a href="'.$url.'">'.$name.'</a></li>
+';
+}
+
+function navbar($title, $active) {
+echo '
+<nav class="navbar navbar-inverse navbar-fixed-top">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">'.$title.'</a>
+		</div>
+
+		<div id="navbar" class="collapse navbar-collapse">
+			<ul class="nav navbar-nav">
+';
+				navelem("Home",   "/",      $active == 1);
+				navelem("Goats",  "/goats", $active == 2);
+				navelem("Page 3", "#blah",  $active == 3);
+				navelem("Page 4", "#blah2", $active == 4);
+echo '			</ul>
+		</div>
+	</div>
+</nav>
+';
+}
+
+function bootstrap_js() {
+echo '
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+';
+}
+?>
---
+++ htdocs/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Maaatch.com</title>
+		<?php require '/var/www/maaatch.com/htdocs/common/common.php'; ?>
+		<?php head_tags(); ?>
+	</head>
+	<body>
+		<?php navbar("Welcome", 1); ?>
+		<main class="container">
+			<article class="welcome-text">
+				<h1>Welcome to Maaatch.com!</h1>
+				<p class="lead">Your first choice in mail-order goats.
+				<br/>
+				<br/>
+				We provide a dating-site feature set to match you with the goat
+				of your dreams. Users will be able to create a profile (chosen
+				by our proprietary matching al<i>goat</i>rithm) with their goat
+				preferences and browse goat profiles. Once a user selects a
+				goat, they will be able to place an order or add to their wish
+				list. Allow 4 to 6 weeks for delivery.
+				</p>
+			</article>
+		</main>
+		<?php bootstrap_js(); ?>
+	</body>
+</html>
